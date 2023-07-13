9cba88b0110267d2e1d057efcf21b2a2257d4973
Michal Lesiak <lesiutrn@gmail.com>
2023-07-13 14:04:14 +0200

    Pop-upy

---
+++ functions.js
@@ -0,0 +1,21 @@
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active");
+}
+
+function togglePopup2(){
+    document.getElementById("popup-2").classList.toggle("active");
+}
+
+function togglePopup3(){
+    document.getElementById("popup-3").classList.toggle("active");
+}
+
+//TODO  [ESC] wyłącza popup - niepoprawne działanie
+/*
+document.addEventListener('keydown', function(event){
+	if(event.key === "Escape"){
+document.getElementById("popup-1").classList.toggle("active");
+
+	}
+}); */
+
